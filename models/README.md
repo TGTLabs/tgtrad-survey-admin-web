7635df436c2481724f4af9f845b839956daa7b21
Donnie Armstrong <donaldjarmstrong@gmail.com>
2014-08-29 09:34:20 -0500

    removing models; trying a different subtree strat

--- models/README.md
+++
@@ -1,4 +0,0 @@
-tgtrad-survey-api
-===============
-
-API schema info in repo wiki
--- models/api/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-exports.register = function (server) {
-  require('./resources/root').register(server);
-  require('./resources/survey').register(server);
-  require('./resources/completedSurvey').register(server);
-  require('./resources/user').register(server);
-};
