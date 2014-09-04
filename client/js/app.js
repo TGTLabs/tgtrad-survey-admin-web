b4a02417a1831eaae200a1e9e7cf040b17bb3397
Donnie Armstrong <donaldjarmstrong@gmail.com>
2014-09-04 16:02:45 -0500

    converted to ui-router; state based

--- client/js/app.js
+++ client/js/app.js
@@ -1,22 +1,50 @@
 'use strict';
+//
+//(function () {
+//  var module = angular.module('angular-client-side-auth', ['ngRoute', 'ngCookies', 'ui.bootstrap']);
+//
+//  module.config(['$routeProvider',
+//    function($routeProvider) {
+//      $routeProvider.
+//        when('/survey', {
+//          templateUrl: 'partials/survey',
+//          controller: 'SurveyController'
+//        }).
+//        when('/survey/imageQuestion', {
+//          templateUrl: 'partials/imageQuestion',
+//          controller: 'QuestionController'
+//        }).
+//        otherwise({
+////          redirectTo: '/survey'
+//          redirectTo: '/'
+//        });
+//    }]);
+//}());
 
 (function () {
-  var module = angular.module('angular-client-side-auth', ['ngRoute', 'ngCookies', 'ui.bootstrap']);
+  var module = angular.module('angular-client-side-auth', ['ui.router', 'ngCookies', 'ui.bootstrap']);
 
-  module.config(['$routeProvider',
-    function($routeProvider) {
-      $routeProvider.
-        when('/survey', {
-          templateUrl: 'partials/survey',
-          controller: 'SurveyController'
-        }).
-        when('/survey/imageQuestion', {
-          templateUrl: 'partials/imageQuestion',
-          controller: 'QuestionController'
-        }).
-        otherwise({
-//          redirectTo: '/survey'
-          redirectTo: '/'
-        });
-    }]);
+  module.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/survey');
+
+    $stateProvider
+      .state('survey', {
+        url: '/survey',
+        templateUrl: 'partials/survey',
+        controller: 'SurveyController'
+      })
+
+      .state('question', {
+        url: '/survey/imageQuestion',
+        templateUrl: 'partials/imageQuestion',
+        controller: 'QuestionController'
+      })
+
+      .state('about', {
+        // we'll get to this in a bit
+      });
+
+  });
+
 }());
